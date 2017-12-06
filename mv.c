12471b7376ca7a2c03622486793f129c533643a1
Unknown <iqbal.izzulhaq3@gmail.com>
2017-12-06 20:27:04 +0700

    if pertama

---
+++ mv.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main (int argc, char *argv[]){
+  if (argc != 3){
+    printf(2, "Usage: mv oldname newname\n");
+    exit();
+  }
+ 
+}
